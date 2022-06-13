0a9f9ddb4a2265a5b7666cbd25b1bb3c44e725a3
Evans Rutto <robbyevans001@gmail.com>
2022-06-13 23:30:21 +0300

    commit tree 6a40a9efd7f5bf14f18218b085369dd95d5dfd67

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded",replaceText
+);
+
+function replaceText(){
+  const text=document.querySelector('#text');
+  text.innerHTML="This is really cool!"
+}
